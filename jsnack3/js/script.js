b6d7787919d24c9257172e1aabeb72700286a6de
MayaSelena773 <maya.bono721@gmail.com>
2022-06-09 16:54:48 +0200

    snack3done

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -2,20 +2,27 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
 
- const userNumber = prompt("Scrivi un numero");
+//Dichiaro la variabile
  let sumNumber = 0;
  
  //formo l'array nel quale verranno aggiunti i numeri dell'utente
  let array = [];
 
  //Ripeto la richiesta per 10 volte
- for (i = 0; i < 11; i++) {
-     let parseInt (userNumber = prompt("Scrivi un numero"));
+ for (i = 0; i < 10; i++) {
+     let userNumber = prompt("Scrivi un numero");
 
      //Aggiungo ogni numero di volta in volta
      array.push(userNumber);
 
-     sumNumber += userNumber;
+     for (let i = 0; i < userNumber.length; i++) {
+
+        const singleNumber = parseInt(userNumber[i]);
+    
+        sumNumber += singleNumber;
+    
+     }
+
  }
  //Stampo in console il risultato finale
  console.log(sumNumber);
